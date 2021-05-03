8c97f534ebb88a4d1755f5dde947e768697cd546
trampimi <mira.trampitsch02@htl-salzburg.ac.at>
2021-05-03 09:06:40 +0200

    interactableobject script und sprites hinzugefuegt

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class InteractableObject : MonoBehaviour
+{
+
+    public Text ObjectText;
+    public UnityEvent clicked;
+
+    private void OnMouseDown()
+    {
+        ObjectText.text = this.gameObject.name;
+        this.gameObject.SetActive(false);
+
+        clicked.Invoke();
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Transform thisTransform;
+    public Vector3 mousePos;
+    public Vector3 mouseCameraPos;
+    public Vector3 playerPos;
+    public float speed = 0.1f;
+
+
+    void Start()
+    {
+        playerPos = thisTransform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mousePos = Input.mousePosition;
+            mouseCameraPos = Camera.main.ScreenToWorldPoint(mousePos);
+            playerPos = new Vector3(mouseCameraPos.x, mouseCameraPos.y, thisTransform.position.z);
+        }
+
+        thisTransform.position = Vector3.MoveTowards(thisTransform.position, playerPos, speed);
+    }
+}
