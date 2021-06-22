7d7b1eec970751669fbc9e751b5d8cddce3c8ee0
trampimi <mira.trampitsch02@htl-salzburg.ac.at>
2021-06-22 12:52:06 +0200

    SwitchScene StartScript und weiter Button Script

---
+++ Assets/Scripts/Weiter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weiter : MonoBehaviour
+{
+    public GameObject blase1;
+    public GameObject blase2;
+    public GameObject blase3;
+    public GameObject blase4;
+
+    public int counter;
+
+
+    public void ClickWeiter()
+    {
+        blase1.SetActive(false);
+        counter = +1;
+
+        if (counter ==1)
+
+        {
+            blase2.SetActive(false);
+            counter = +1;
+        }
+     
+    }
+   
+}
